87be6439bdd63cccf5ad930486cc8644b60c4d1b
Zero <yasseravalosmontero14@gmail.com>
2023-11-08 01:16:35 -0500

    Proyecto MLNET

---
+++ Controllers/IAController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMLApp;
+
+namespace SentimentalAIWeb.Controllers
+{
+    public class IAController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AnalizarTexto(string textInput)
+        {
+            //Load sample data
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = textInput,
+            };
+
+            //Load model and predict output
+            var result = SentimentModel.Predict(sampleData);
+
+            return View("Index", result);
+        }
+
+    }
+}
